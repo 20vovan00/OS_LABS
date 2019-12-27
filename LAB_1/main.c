27a6bbd22f8afb70129c8251d670f91acfb02364
20vovan00 <48915469+20vovan00@users.noreply.github.com>
2019-12-27 15:01:01 +0300

    Update main.c

--- LAB_1/main.c
+++ LAB_1/main.c
@@ -1,75 +1,115 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-void FuncForAtExit()
-{
-    printf("Procces %d - happy ended (atexit())\n", getpid());
-}
 
-void FuncForOnExit()
-{
-    printf("Procces %d - happy ended (on_exit())\n", getpid());
-}
-int zero = 0;
-int main(int argc, char **argv)
-{
-    atexit(FuncForAtExit);
-    on_exit(FuncForOnExit, NULL);
-    pid_t  fpid;
-    pid_t spid;
-    int status;
-    int rv;
-    int j = 3;
-    fpid = fork();
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
 
-    printf("fpid from fork: %d\n", fpid);
-    switch (fpid)
-    {
-        case -1:
-            perror("fork");
-            exit(1);
-        case 0:
-            printf("CHILD(%d): PID моего родителя -- %d\n", getpid(), getppid());
-            printf("CHILD(%d): Введите мой код возврата (как можно меньше):", getpid());
-            scanf("%d", &rv);
-            printf("CHILD(%d): Выход!\n", getpid());
-            exit(rv);
-        default:
-            printf("PARENT(%d): PID моего потомка -- %d\n", getpid(), fpid);
-            printf("PARENT(%d): Я жду, пока потомок не вызовет exit()...\n", getpid());
-            wait(&status);
-            printf("PARENT(%d): Код возврата потомка: %d\n", getpid(), WEXITSTATUS(status));
-            printf("PARENT(%d): Статус выхода потомка -- %d\n", getpid(), status);
-            printf("PARENT(%d): Выход!\n", getpid());
-    }
+#include <time.h>
+#include <signal.h>
 
-    spid = fork();
+int main(){
+	
+	int fd;
+	pid_t pid;
+	char *fifo = "/tmp/fif_1";
+	if(mkfifo(fifo, 0666) == -1)
+	{
+		perror("Fifo error\n"); 
+		return 1;
+	}
 
-    printf("spid from fork: %d\n", spid);
-    switch (spid)
-    {
-        case -1:
-            perror("fork");
-            exit(1);
-        case 0:
-            j = 7/zero;
-            printf("CHILD(%d): PID моего родителя -- %d\n", getpid(), getppid());
-            printf("CHILD(%d): Введите мой код возврата (как можно меньше):", getpid());
-            scanf("%d", &rv);
-            printf("CHILD(%d): Выход!\n", getpid());
-            exit(rv);
-        default:
-            printf("PARENT(%d): PID моего потомка -- %d\n", getpid(), spid);
-            printf("PARENT(%d): Я жду, пока потомок не вызовет exit()...\n", getpid());
-            wait(&status);
-            printf("PARENT(%d): Код возврата потомка: %d\n", getpid(), WEXITSTATUS(status));
-            printf("PARENT(%d): Статус выхода потомка -- %d\n", getpid(), status);
-            printf("PARENT(%d): Выход!\n", getpid());
-    }
+	
+	pid = fork();
 
-    return 0;
+
+		struct tm *tt;
+
+	switch(pid)
+	{
+		case -1:
+			perror("fork error\n"); return 1;
+		case 0:
+		{
+			char buf[30]; int len = 0;
+			time_t tmt = time(NULL);
+			tt = localtime(&tmt);
+			
+			strftime(buf, sizeof(buf), "%T", tt);
+			sprintf(buf, "%s", buf);
+	
+
+			fd = open(fifo, O_WRONLY);			
+			len = write(fd,buf,30);
+			
+			memset(buf,'\0',30);
+				
+			close(fd);
+			if(len > 0)
+			{
+				printf("Children await\n");
+				kill(getpid(),SIGSTOP); 
+			}
+
+			fd = open(fifo,O_RDONLY);		
+			len = read(fd, buf, 30);
+			if(len > 0)	
+				printf("PARENT TIME: %s\n", buf);
+			else perror("No response from parent\n");
+	
+			close(fd);
+
+			unlink(fifo);
+			exit(0);
+		}
+		default:
+		{
+			int len = 0;
+			char buf[30];
+			
+			fd = open(fifo,O_RDONLY);
+
+			len = read(fd,buf,30);
+
+		if(len > 0)					
+			printf("CHILD TIME: %s\n", buf);
+		else perror("No response from children\n");
+
+			memset(buf,'\0',30);
+				sleep(1);
+
+	
+			close(fd);
+		
+            
+                time_t tmt = time(NULL);
+                tt = localtime(&tmt);
+
+				
+			strftime(buf, sizeof(buf), "%T", tt);
+			sprintf(buf, "%s", buf);
+	
+			sleep(1);	
+			kill(pid, SIGCONT); 
+
+			fd = open(fifo, O_WRONLY);			
+			
+				len = write(fd,buf,30);
+				printf("Writing... \n");
+	
+				close(fd);
+
+			printf("Closing p\n");
+			unlink(fifo);
+			exit(0);
+		}
+	}
+
+	
+	return 0;
 }
